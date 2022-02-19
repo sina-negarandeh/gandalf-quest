55208b17a18d5544f7596ccfaeec7969c19ffd1c
sina-negarandeh <negarandeh.sina@gmail.com>
2022-02-19 16:13:17 +0330

    Add source files

---
+++ src/bfs.py
@@ -0,0 +1,31 @@
+import utils
+import queue
+import time
+import os
+
+
+def BFS(initial_state):
+    explored = set()
+    frontier = queue.Queue()
+    frontier.put(initial_state)
+    explored.add(initial_state)
+    
+    while not frontier.empty():
+        state = frontier.get()
+        if state.goal_test():
+            return state
+
+        new_states = state.new_states()
+
+        for new_state in new_states:
+            if new_state not in explored:
+                frontier.put(new_state)
+                explored.add(new_state)
+    return None
+
+
+initial_state = utils.read_from_file("./tests/test_07.txt")
+tic = time.time()
+print(BFS(initial_state).path)
+toc = time.time()
+print("Time: %f ms" % ((toc - tic) * 1000))
